22c6cb9dfc9b605d5c94e7b34fed5c1bbb253696
Etienne Dauvergne <contact@ekyna.com>
2015-07-21 16:13:39 +0200

    Removed form options default value.

--- Form/Type/RedirectionType.php
+++ Form/Type/RedirectionType.php
@@ -15,7 +15,7 @@ class RedirectionType extends ResourceFormType
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options = array())
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('fromPath', 'text', array(
